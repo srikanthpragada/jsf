b50a0965d4c14559d74d4edba9c79eab2acc2ae3
Srikanth Pragada <srikanthpragada@gmail.com>
2016-03-22 19:58:50 +0530

    JobsBeans to retrieve data from Database

--- JobsBean.java
+++ JobsBean.java
@@ -22,5 +22,27 @@ public class JobsBean {
 			return null;
 		}
 	}
+	
+	public List<Job>  getAllJobs() {
+		try {
+			CachedRowSet crs = new OracleCachedRowSet();
+			crs.setUsername("hr");
+			crs.setPassword("hr");
+			crs.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+			crs.setCommand("select * from jobs");
+			crs.execute();
+			ArrayList<Job> jobs  = new ArrayList<>();
+			while(crs.next()) {
+				Job j = new Job();
+				j.setId( crs.getString("job_id"));
+				j.setTitle( crs.getString("job_title"));
+				jobs.add(j);
+			}
+			crs.close();
+			return jobs;
+		} catch (Exception ex) {
+			return null;
+		}
+	}
 
 }
